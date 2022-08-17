74530aaabcba46edc0042c2ab672020c71ef5cd9
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-17 16:46:58 +0900

    [Finish]1012

---
+++ Ready1011.java
@@ -0,0 +1,7 @@
+public class Ready1011 {
+  public static void main(String[] args) {
+  int x = 10;
+
+  System.out.println(x / 4 + "," + x % 4);
+  }
+}
---
+++ Ready1012.java
@@ -0,0 +1,13 @@
+public class Ready1012 {
+  public static void main(String[] args) {
+  int x = 10;
+
+  x++;
+
+  System.out.println(x);
+
+  x--;
+  x--;
+  System.out.println(x);
+  }
+}
