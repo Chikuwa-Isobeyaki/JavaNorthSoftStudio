56da0ccea28559480f3cccb2193a208c119df01b
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-17 16:38:49 +0900

    [Finish]1010

---
+++ Ready109.java
@@ -0,0 +1,9 @@
+public class Ready109 {
+  public static void main(String[] args) {
+  int x = 10;
+
+  System.out.println(x * 2);
+  System.out.println(x * 3);
+  System.out.println(x * 4);
+  }
+}
