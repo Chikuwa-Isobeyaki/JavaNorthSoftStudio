40025b3032ddc9d7e46a9a49df0f4925840be4af
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-17 16:27:33 +0900

    [Finish]108

---
+++ Ready107.java
@@ -0,0 +1,13 @@
+public class Ready107 {
+  public static void main(String[] args) {
+  int x = 3;
+  int y = 7;
+
+  int z = x;
+  x = y;
+  y = z;
+
+
+  System.out.println( "x=" + x + ",y=" + y );
+  }
+}
