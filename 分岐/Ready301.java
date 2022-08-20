7232f8c959fb6bf0abf71847eacf64456381f9aa
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-20 14:55:10 +0900

    [Finish]3-2

---
+++ 分岐/Ready301.java
@@ -0,0 +1,10 @@
+public class Ready301 {
+    public static void main(String[] args) {
+        int x = 9;
+        int y = 8;
+
+        if(x > y) {
+          System.out.println("xはyより大きい");
+        }
+    }
+}
---
+++ 分岐/Ready302.java
@@ -0,0 +1,12 @@
+public class Ready302 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 8;
+
+        if(x > y) {
+          System.out.println("xはyより大きい");
+        } else {
+          System.out.println("yはxより大きい");
+        }
+    }
+}
