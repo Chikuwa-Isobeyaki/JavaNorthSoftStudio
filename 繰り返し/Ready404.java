1ee9e5158708eb4ae1c456b0c0c4e4465a26bec1
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-20 17:34:26 +0900

    [Finish]404

---
+++ 繰り返し/Ready404.java
@@ -0,0 +1,11 @@
+public class Ready404 {
+  public static void main(String[] args) {
+    int x = 1;
+
+    for( int i = 2 ; i <= 7 ; i++ ) {
+      x *= i;
+    }
+
+    System.out.println( x );
+  }
+}
